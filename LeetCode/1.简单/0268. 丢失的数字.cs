95627bdc3dbf4f773ed159b0080ade70d3e7b82a
UntaughtMinds <25280272+UntaughtMinds@users.noreply.github.com>
2021-01-27 09:18:13 +0800

    完成0268

---
+++ LeetCode/1.简单/0268. 丢失的数字.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+namespace LeetCode
+{
+    public class Question0268
+    {
+        /*
+         * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+         */
+
+        [Theory]
+        [Q0268TestData]
+        public void Test(int[] input, int output)
+        {
+            Assert.Equal(output, MissingNumberPlus(input));
+        }
+
+        public int MissingNumber(int[] nums)
+        {
+            int size = nums.Length;
+            int Res = 0;
+
+            for (int i = 0; i <= size; i++)
+            {
+                Res ^= i;
+            }
+
+            return nums.Aggregate(Res, (current, num) => current ^ num);
+        }
+
+        public int MissingNumberPlus(int[] nums)
+        {
+            int size = nums.Length;
+            var Res = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                Res ^= i ^ nums[i];
+            }
+
+            Res ^= size;
+
+            return Res;
+        }
+
+    }
+
+    public class Q0268TestData : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { new object[] { 3, 0, 1 }, 2 };
+            yield return new object[] { new object[] { 0, 1 }, 2 };
+            yield return new object[] { new object[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8 };
+            yield return new object[] { new object[] { 0 }, 1 };
+        }
+    }
+}
