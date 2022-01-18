735f90c5f3b21c4840b390d2913c82a9e3884e40
UntaughtMinds <25280272+UntaughtMinds@users.noreply.github.com>
2022-01-18 09:38:15 +0800

    完成0539、1220

---
+++ LeetCode/2.中等/0539. 最小时间差.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+namespace LeetCode
+{
+    public class Question0539
+    {
+        /*
+         * 给定一个 24 小时制（小时:分钟 "HH:MM"）的时间列表，找出列表中任意两个时间的最小时间差并以分钟数表示。
+         */
+
+        [Theory]
+        [Q0539TestData]
+        public void Test(IList<string> input, int output)
+        {
+            Assert.Equal(output, FindMinDifference(input));
+        }
+
+        public int FindMinDifference(IList<string> timePoints)
+        {
+            if (timePoints.Count > 1440)
+                return 0;//鸽巢原理
+
+            int min = 99999999;
+            for(int i = 0; i < timePoints.Count; i++)
+            {
+                string[] time1 = timePoints[i].Split(':');
+                for (int j = i + 1; j < timePoints.Count; j++)
+                {
+                    string[] time2 = timePoints[j].Split(':');
+                    int hour1 = int.Parse(time1[0]);
+                    int hour2 = int.Parse(time2[0]);
+                    int minutes1= int.Parse(time1[1]);
+                    int minutes2 = int.Parse(time2[1]);
+                    for (int k = 0; k < 2; k++)
+                    {
+                        if (hour1 < hour2 ||( hour1==hour2 && minutes1 < minutes2))
+                        {
+                            int temp = hour1;
+                            hour1 = hour2;
+                            hour2 = temp;
+                            temp = minutes1;
+                            minutes1 = minutes2;
+                            minutes2 = temp;
+                        }
+
+                        //计算时间差
+                        if (minutes1 < minutes2)
+                        {
+                            hour1 -= 1;
+                            minutes1 += 60;
+                        }
+                        int minutes = minutes1 - minutes2;
+                        minutes += (hour1 - hour2) * 60;
+                        if (min > minutes)
+                        {
+                            min = minutes;
+                        }
+
+                        if (k == 0)
+                        {
+                            hour2 += 24;
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+
+            return min;
+        }
+
+    }
+
+    public class Q0539TestData : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { new List<string>() { "23:59", "00:00" }, 1 };
+            yield return new object[] { new List<string>() { "00:00", "23:59", "00:00" }, 0 };
+        }
+    }
+}
---
+++ LeetCode/3.困难/1220. 统计元音字母序列的数目.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+namespace LeetCode
+{
+    public class Question1220
+    {
+        /*
+         * 给你一个整数 n，请你帮忙统计一下我们可以按下述规则形成多少个长度为 n 的字符串：
+         * 字符串中的每个字符都应当是小写元音字母（'a', 'e', 'i', 'o', 'u'）
+         * 每个元音 'a' 后面都只能跟着 'e'
+         * 每个元音 'e' 后面只能跟着 'a' 或者是 'i'
+         * 每个元音 'i' 后面 不能 再跟着另一个 'i'
+         * 每个元音 'o' 后面只能跟着 'i' 或者是 'u'
+         * 每个元音 'u' 后面只能跟着 'a'
+         * 由于答案可能会很大，所以请你返回 模 10^9 + 7 之后的结果。。
+         */
+
+        [Theory]
+        [Q1220TestData]
+        public void Test(int input, int output)
+        {
+            Assert.Equal(output, CountVowelPermutation(input));
+        }
+
+        public int CountVowelPermutation(int n)
+        {
+            long mod = (long)Math.Pow(10, 9) + 7;
+
+            long[] list = new long[5];
+            long[] newlist = new long[5];
+
+            long res = 0;
+
+            for (int i = 0;i<5;i++)
+                list[i] = 1;
+
+            for (int j = 1;j<n;j++)
+            {
+                newlist[0] = (list[1] + list[2] + list[4]) % mod;
+                newlist[1] = (list[0] + list[2]) % mod;
+                newlist[2] = (list[1] + list[3]) % mod;
+                newlist[3] = (list[2]) % mod;
+                newlist[4] = (list[2] + list[3]) % mod;
+
+                list[0] = newlist[0];
+                list[1] = newlist[1];
+                list[2] = newlist[2];
+                list[3] = newlist[3];
+                list[4] = newlist[4];
+            }
+
+            for(int i = 0;i<5; i++)
+            {
+                res += list[i];
+                res %= mod;
+            }
+
+            return (int)res;
+        }
+
+    }
+
+    public class Q1220TestData : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { 1, 5 };
+            yield return new object[] { 2, 10 };
+            yield return new object[] { 5, 68 };
+            yield return new object[] { 144, 18208803 };
+        }
+    }
+}
